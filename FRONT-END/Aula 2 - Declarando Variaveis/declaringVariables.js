2e742997072b7fbc98d3919bb15e45636742cf78
Guhzxs <gustavo4parecido@gmail.com>
2026-07-07 23:41:55 -0300

    Aula 2 - Declarando Variaveis

---
+++ FRONT-END/Aula 2 - Declarando Variaveis/declaringVariables.js
@@ -0,0 +1,18 @@
+// var let const (formas de declarar variáveis)
+
+const primeiroValor = 10;
+const segundoValor = 20;
+
+console.log("Imprimindo o primeiroValor + segundoValor", primeiroValor + segundoValor);
+
+const valorTotal = primeiroValor + segundoValor;
+
+console.log("Imprimindo variavel valorTotal", valorTotal);
+
+let terceiroValor = 50;
+console.log("terceiroValor: ", terceiroValor);
+
+terceiroValor = 100;
+console.log("terceiroValor final:", terceiroValor);
+
+//no caso utilizamos o let quando a variável não possuir valor fixo
